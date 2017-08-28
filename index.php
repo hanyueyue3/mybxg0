9565ccaae1d09cd37343aa7cb67e79ac6d24eb83
hanyueyue <hanyueyue.1988@163.com>
2017-08-28 17:48:52 +0800

    初始化仓库 版本1.0.0

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$dir ='main';
+$filename='index';
+if(array_key_exists('PATH_INFO',$_SERVER)){
+	$path=$_SERVER['PATH_INFO'];
+	$str=substr($path,1);
+	$arr=explode('/',$str);
+	if(count($arr)==2){
+		$dir=$arr[0];
+		$filename=$arr[1];
+	}else{
+		$filename='login';
+	}
+}
+	include('./views/'.$dir.'/'.$filename.'.html');
+
+?>
---
+++ public/js/common.js
@@ -0,0 +1,24 @@
+
+	NProgress.start();
+
+	NProgress.done();
+
+	$('.navs ul').prev('a').on('click', function () {
+		$(this).next().slideToggle();
+	});
+
+	// 实现退出功能
+	$('#logoutBtn').click(function(){
+		console.log(123)
+        $.ajax({
+            type:'post',
+            url:'/api/logout',
+            dataType:'json',
+            success:function(data){
+                if(data.code==200){
+                    location.href='/main/login'
+                }
+            }
+        })
+        return false;
+    })
